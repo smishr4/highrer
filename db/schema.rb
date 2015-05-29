f7bef26a9d60be1e2d80091437871b4682a9e38a
Shubham Mishra <shubham.mishra@tinyowl.co.in>
2015-05-30 03:29:31 +0530

    apis added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529181407) do
+ActiveRecord::Schema.define(version: 20150529212832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150529181407) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.text     "friend_ids"
+    t.integer  "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
